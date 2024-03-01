9d75842ff4baea367d61f9ba84f7de63a1511f12
Jovani Matthieu <73386424+jovani494@users.noreply.github.com>
2024-03-01 19:22:47 +0300

    Update environements.ts

--- src/environements/environements.ts
+++ src/environements/environements.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // apiUrl: 'https://apimean-jovani-idealy.onrender.com' // Définissez l'URL de votre API ici
     // apiUrl: 'http://localhost:8080' 
-    apiUrl : 'https://node-api-jovani-idealy.onrender.com'
-    // apiUrl : 'https://node-back-production.up.railway.app'
+    // apiUrl : 'https://node-api-jovani-idealy.onrender.com'
+    apiUrl : 'https://node-back-production.up.railway.app'
   };
